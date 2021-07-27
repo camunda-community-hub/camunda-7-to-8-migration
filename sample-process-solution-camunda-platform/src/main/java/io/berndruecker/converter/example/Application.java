0c68732e30532710cbdb0e5df784b63fd66f0bd1
Bernd Ruecker <bernd.ruecker@camunda.com>
2021-07-27 16:14:39 +0200

    Added Camunda Platform Sample

---
+++ sample-process-solution-camunda-platform/src/main/java/io/berndruecker/converter/example/Application.java
@@ -0,0 +1,18 @@
+package io.berndruecker.converter.example;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.variable.Variables;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String... args) {
+    ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+    ProcessEngine engine = context.getBean(ProcessEngine.class);
+    engine.getRuntimeService().startProcessInstanceByKey("sample-process-solution-process", Variables.createVariables().putValue("x", 7));
+  }
+
+}
---
+++ sample-process-solution-camunda-platform/src/main/java/io/berndruecker/converter/example/SampleBean.java
@@ -0,0 +1,12 @@
+package io.berndruecker.converter.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleBean {
+
+    public void someMethod() {
+        System.out.println("SampleBean.someMethod()");
+    }
+
+}
