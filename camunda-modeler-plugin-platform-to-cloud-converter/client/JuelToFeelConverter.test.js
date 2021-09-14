363b9961d5fed6981853d2447b8e856eeeb135e3
Bernd Ruecker <bernd.ruecker@camunda.com>
2021-09-14 12:51:59 +0200

    removed unused files

--- camunda-modeler-plugin-platform-to-cloud-converter/client/JuelToFeelConverter.test.js
+++ camunda-modeler-plugin-platform-to-cloud-converter/client/JuelToFeelConverter.test.js
@@ -31,6 +31,7 @@ test("Simple Expressions", () => {
 });
 
 notest("Objects", () => {
+    // This is still to be implemented
     expect(convertJuel("#{customer.name}").feelExpression).toBe("=customer.name");
     expect(convertJuel('#{customer.address["street"]}').feelExpression).toBe("=customer.address.street");
     expect(convertJuel("#{customer.orders[1]}").feelExpression).toBe("=customer.orders[2]");
@@ -38,18 +39,19 @@ notest("Objects", () => {
 
 
 notest("JUEL Tutorial", () => {
-    // https://docs.oracle.com/javaee/5/tutorial/doc/bnahq.html
-    expect(convertJuel("${1 > (4/2)}").feelExpression).toBe("=customer.name");
-    expect(convertJuel("${4.0 >= 3}").feelExpression).toBe("=customer.name");
-    expect(convertJuel("${100.0 == 100}").feelExpression).toBe("=customer.name");
-    expect(convertJuel("${(10*10) ne 100}").feelExpression).toBe("=customer.name");
-    expect(convertJuel("${'a' < 'b'}").feelExpression).toBe("=customer.name");
-    expect(convertJuel("${'hip' gt 'hit'}").feelExpression).toBe("=customer.name");
-    expect(convertJuel("${4 > 3}").feelExpression).toBe("=customer.name");
-    expect(convertJuel("${1.2E4 + 1.4}").feelExpression).toBe("=customer.name");
-    expect(convertJuel("").feelExpression).toBe("=customer.name");
-    expect(convertJuel("").feelExpression).toBe("=customer.name");
-    expect(convertJuel("").feelExpression).toBe("=customer.name");
+    // Other elements valid in JUEL (taken from https://docs.oracle.com/javaee/5/tutorial/doc/bnahq.html)
+    // Still need to be translated and implemented
+    expect(convertJuel("${1 > (4/2)}").feelExpression).toBe("=xxx");
+    expect(convertJuel("${4.0 >= 3}").feelExpression).toBe("=xxx");
+    expect(convertJuel("${100.0 == 100}").feelExpression).toBe("=xxx");
+    expect(convertJuel("${(10*10) ne 100}").feelExpression).toBe("=xxx");
+    expect(convertJuel("${'a' < 'b'}").feelExpression).toBe("=xxx");
+    expect(convertJuel("${'hip' gt 'hit'}").feelExpression).toBe("=xxx");
+    expect(convertJuel("${4 > 3}").feelExpression).toBe("=xxx");
+    expect(convertJuel("${1.2E4 + 1.4}").feelExpression).toBe("=xxx");
+    expect(convertJuel("").feelExpression).toBe("=xxx");
+    expect(convertJuel("").feelExpression).toBe("=xxx");
+    expect(convertJuel("").feelExpression).toBe("=xxx");
 });
 
 function notest(name, test) {
--- camunda-modeler-plugin-platform-to-cloud-converter/dist/client.js
+++ camunda-modeler-plugin-platform-to-cloud-converter/dist/client.js
@@ -343,7 +343,15 @@ ConvertToCamundaCloudPlugin.prototype.convertToCamundaCloud = function() {
       hints = convertXorGateway(element);
     } else if (element.type == "bpmn:SequenceFlow") {
       hints = convertSequenceFlow(element);
+    } else if (element.type == "bpmn:ScriptTask") {
+      hints = convertScriptTask(element);
+    } else if (element.type == "bpmn:ReceiveTask") {
+      hints = convertReceiveTask(element);
+    } else if (element.type == "bpmn:BusinessRuleTask") {
+      hints = convertBusinessRuleTask(element);
     }
+
+    
   ///////////////////////////////////////////////////////////////////////////
 
     if (hints && hints.length > 0) {
@@ -393,6 +401,15 @@ function addExtensionElement(element, extensionElement) {
   }
 }
 
+function readExtensionElement(element, extensionElementType) {
+  for (const extensionElementInLoop of element.businessObject.extensionElements.get('values')) {
+    if (extensionElementInLoop.$type == extensionElementType) { 
+      return extensionElementInLoop;
+    }
+  }
+  return null;
+}
+
 function createTaskDefinition(element, taskType) {
   var taskDef = addExtensionElement(element, moddle.create("zeebe:TaskDefinition"));
   taskDef.type = taskType;  
@@ -424,7 +441,19 @@ function addTaskHeader(element, key, value) {
   }} element 
  */
 
+  function unsupportedElement(hints, elementType, extensionAttribute) {
+    hints.push("Element " + extensionAttribute + " of " + elementType + " not supported");
+  }
+  function unsupportedAttribute(hints, elementType, extensionAttribute) {
+    hints.push("Attribute " + extensionAttribute + " of " + elementType + " not supported (what does it do? See https://docs.camunda.org/manual/latest/reference/bpmn20/custom-extensions/extension-attributes/#"+extensionAttribute+")");
+  }
+  function unsupportedExtensionElement(hints, elementType, extensionElement) {
+    hints.push("Element " + extensionElement + " of " + elementType + " not supported (what does it do? See https://docs.camunda.org/manual/latest/reference/bpmn20/custom-extensions/extension-elements/#"+extensionElement+")");
+  }
+  
+  
 /**
+ * ###############################################
  * CONVERTION METHODS FOR VARIOUS ELEMENT TYPES
  * ###############################################
  */
@@ -435,6 +464,12 @@ function convertDefinitions(rootElement) {
   definitionsElement.set('modeler:executionPlatformVersion', '1.1.0')  
 }
 
+/**
+ * ##################
+ * # Service Task
+ * # https://docs.camunda.org/manual/7.15/reference/bpmn20/tasks/service-task/
+ * ##################
+ */
 function convertServiceTask(element) {
   var hints = [];
   console.log("------------ Service Task -----------------");
@@ -448,12 +483,30 @@ function convertServiceTask(element) {
   } else if (element.businessObject.expression) { // ```camunda:expression```
     createTaskDefinition(element, "camunda-platform-to-cloud-migration");
     addTaskHeader(element, "expression", element.businessObject.expression);
+    addTaskHeader(element, "resultVariable", element.businessObject.resultVariable);
   } else if (element.businessObject.topic) { // External Tasks
     createTaskDefinition(element, element.businessObject.topic);
   }
-  // TODO: IO, Field Injection, Expressions
-  if (!element.businessObject.asyncBefore || !element.businessObject.asyncAfter) {
-    hints.push("Service tasks are all 'async' in Camunda Cloud")
+
+  if (!element.businessObject.asyncBefore || !element.businessObject.asyncAfter) {hints.push("Service tasks are all 'async' in Camunda Cloud");}
+  // if (element.businessObject.exclusive) Exclusive is ignored - as all jobs are automatically exclusive in Camunda Cloud
+  if (element.businessObject.jobPriority) {unsupportedAttribute(hints, "Service Task", "jobPriority");}
+  if (element.businessObject.taskPriority) {unsupportedAttribute(hints, "Service Task", "taskPriority");}
+  if (element.businessObject.type) {unsupportedAttribute(hints, "Service Task", "type");}
+
+  if (readExtensionElement(element, "failedJobRetryTimeCycle")) {unsupportedExtensionElement(hints, "Service Task", "failedJobRetryTimeCycle");}
+  if (readExtensionElement(element, "connector")) {unsupportedExtensionElement(hints, "Service Task", "connector");}
+  if (errorEventDefinition = readExtensionElement(element, "errorEventDefinition")) {
+    // TODO handle errorEventDefinition
+  }
+  if (field = readExtensionElement(element, "field")) {
+    // TODO handle field
+  }
+  if (inputOutput = readExtensionElement(element, "inputOutput")) {
+    // TODO handle inputOutput
+  }
+  if (taskListener = readExtensionElement(element, "taskListener")) {
+    unsupportedExtensionElement(hints, "Service Task", "taskListener");
   }
 
   console.log(element);
@@ -461,25 +514,39 @@ function convertServiceTask(element) {
   return hints;
 }
 
+/**
+ * ##################
+ * # User Task
+ * # https://docs.camunda.org/manual/7.15/reference/bpmn20/tasks/user-task/
+ * ##################
+ */
 function convertUserTask(element) {
   var hints = [];
   console.log("------------ User Task -----------------");
 
-  if (element.businessObject.class) { // ```camunda:class```
-    createTaskDefinition(element, "camunda-platform-to-cloud-migration");
-    addTaskHeader(element, "class", element.businessObject.class);
-  } else if (element.businessObject.delegateExpression) { // ```camunda:delegateExpression```
-    createTaskDefinition(element, "camunda-platform-to-cloud-migration");
-    addTaskHeader(element, "delegateExpression", element.businessObject.delegateExpression);
-  } else if (element.businessObject.expression) { // ```camunda:expression```
-    createTaskDefinition(element, "camunda-platform-to-cloud-migration");
-    addTaskHeader(element, "expression", element.businessObject.expression);
-  } else if (element.businessObject.topic) { // External Tasks
-    createTaskDefinition(element, element.businessObject.topic);
+  // Assignment
+  if (element.businessObject.humanPerformer) {unsupportedElement(hints, "User Task", "humanPerformer");}
+  if (element.businessObject.potentialOwner) {unsupportedElement(hints, "User Task", "potentialOwner");}
+  if (element.businessObject.assignee) {unsupportedAttribute(hints, "User Task", "assignee");}
+  if (element.businessObject.candidateUsers) {unsupportedAttribute(hints, "User Task", "candidateUsers");}
+  if (element.businessObject.candidateGroups) {unsupportedAttribute(hints, "User Task", "candidateGroups");}
+  
+  // Forms TODO: Think about form migration
+  if (element.businessObject.formKey) {unsupportedAttribute(hints, "User Task", "formKey");}
+  if (element.businessObject.formHandlerClass) {unsupportedAttribute(hints, "User Task", "formHandlerClass");}
+  if (readExtensionElement(element, "formData")) {unsupportedExtensionElement(hints, "Service Task", "formData");}
+  if (readExtensionElement(element, "formProperty")) {unsupportedExtensionElement(hints, "Service Task", "formData");}
+
+  if (element.businessObject.dueDate) {unsupportedAttribute(hints, "User Task", "dueDate");}
+  if (element.businessObject.followUpDate) {unsupportedAttribute(hints, "User Task", "followUpDate");}
+  if (element.businessObject.priority) {unsupportedAttribute(hints, "User Task", "priority");}
+  // Ignore asyncBefore, asyncAfter, exclusive, jobPriority, failedJobRetryTimeCycle - as without listeners that does not make any difference anyway
+
+  if (inputOutput = readExtensionElement(element, "inputOutput")) {
+    // TODO handle inputOutput
   }
-  // TODO: IO, Field Injection, Expressions
-  if (!element.businessObject.asyncBefore || !element.businessObject.asyncAfter) {
-    hints.push("Service tasks are all 'async' in Camunda Cloud")
+  if (taskListener = readExtensionElement(element, "taskListener")) {
+    unsupportedExtensionElement(hints, "Service Task", "taskListener");
   }
 
   console.log(element);
@@ -487,15 +554,41 @@ function convertUserTask(element) {
   return hints;
 }
 
-// TODO: Add hint to model
+
+/**
+ * ##################
+ * # Call Activity
+ * # https://docs.camunda.org/manual/7.15/reference/bpmn20/subprocesses/call-activity/
+ * ##################
+ */
 function convertCallActivity(element) {
   var hints = [];
   console.log("------------ Call Activity -----------------");
 
   if (element.businessObject.calledElement) {
     var calledElementDef = addExtensionElement(element, moddle.create("zeebe:CalledElement"));
-    calledElementDef.processId = element.businessObject.calledElement;
-    //TODO: calledElementDef.propagateAllChildVariables
+
+    var feelResult = convertJuel(element.businessObject.calledElement);
+    calledElementDef.processId = feelResult.feelExpression;
+    hints.push(feelResult.hints);
+  }
+  if (element.businessObject.calledElementBinding) {unsupportedAttribute(hints, "Call Activity", "calledElementBinding");}
+  if (element.businessObject.calledElementVersionTag) {unsupportedAttribute(hints, "Call Activity", "calledElementVersionTag");}
+
+  // Variables
+  // https://docs.camunda.org/manual/7.15/reference/bpmn20/subprocesses/call-activity/#passing-variables
+  // No attribute in Camunda Platform like "calledElementDef.propagateAllChildVariables"
+  if (element.businessObject.variableMappingClass) {unsupportedAttribute(hints, "Call Activity", "variableMappingClass");}
+  if (element.businessObject.variableMappingDelegateExpression) {unsupportedAttribute(hints, "Call Activity", "variableMappingDelegateExpression");}
+  
+  if (inMapping = readExtensionElement(element, "in")) {
+    unsupportedExtensionElement(hints, "Call Activity", "in");
+  }
+  if (outMapping = readExtensionElement(element, "out")) {
+    unsupportedExtensionElement(hints, "Call Activity", "out");
+  }
+  if (inputOutput = readExtensionElement(element, "inputOutput")) {
+    // TODO handle inputOutput
   }
 
   console.log(element);
@@ -517,6 +610,93 @@ function convertSequenceFlow(element) {
   }
 }
 
+/**
+ * ##################
+ * # Script Task
+ * # https://docs.camunda.org/manual/7.15/reference/bpmn20/tasks/script-task/
+ * ##################
+ */
+function convertScriptTask(element) {
+   // see  https://docs.camunda.io/docs/reference/bpmn-processes/script-tasks/script-tasks
+   // This is considered not migratable at the moment, but 
+   // it could also use the copmmunity extension down the line: https://github.com/camunda-community-hub/zeebe-script-worker
+   var hints = [];
+   console.log("------------ Script Task -----------------");
+   
+   var scriptFormat = element.businessObject.scriptFormat;
+
+  createTaskDefinition(element, "script");
+  addTaskHeader(element, "language", element.businessObject.scriptFormat);
+  addTaskHeader(element, "script", element.businessObject.script);
+
+   hints.push("Scripts might not work as expected using the https://github.com/camunda-community-hub/zeebe-script-worker - please double check");
+   
+   console.log("------------ ---------- -----------------");
+   return hints;
+ 
+}
+
+/**
+ * ##################
+ * # Business Rule Task
+ * # 
+ * ##################
+ */
+ function convertBusinessRuleTask(element) {
+  var hints = [];
+  console.log("------------ Business Rule Task -----------------");
+
+  if (element.businessObject.class || element.businessObject.delegateExpression || element.businessObject.expression || element.businessObject.topic) { 
+    // used like a service task
+    convertServiceTask(element);
+  } else {
+    // used for specific DMN integration
+    createTaskDefinition(element, "DMN");
+    addTaskHeader(element, "decisionRef", element.businessObject.decisionRef);
+    
+    if (element.businessObject.decisionRefBinding) {unsupportedAttribute(hints, "Business Rule Task", "decisionRefBinding");}
+    if (element.businessObject.decisionRefVersion) {unsupportedAttribute(hints, "Business Rule Task", "decisionRefVersion");}
+    if (element.businessObject.decisionRefVersionTag) {unsupportedAttribute(hints, "Business Rule Task", "decisionRefVersionTag");}
+    if (element.businessObject.mapDecisionResult) {unsupportedAttribute(hints, "Business Rule Task", "mapDecisionResult");}
+    if (element.businessObject.resultVariable) {unsupportedAttribute(hints, "Business Rule Task", "resultVariable");}
+    if (element.businessObject.decisionRefTenantId) {unsupportedAttribute(hints, "Business Rule Task", "decisionRefTenantId");}
+
+    if (!element.businessObject.asyncBefore || !element.businessObject.asyncAfter) {hints.push("Tasks are all 'async' in Camunda Cloud");}
+    // if (element.businessObject.exclusive) Exclusive is ignored - as all jobs are automatically exclusive in Camunda Cloud
+    if (element.businessObject.jobPriority) {unsupportedAttribute(hints, "Business Rule Task", "jobPriority");}
+    if (element.businessObject.taskPriority) {unsupportedAttribute(hints, "Business Rule Task", "taskPriority");}
+    if (element.businessObject.type) {unsupportedAttribute(hints, "Business Rule Task", "type");}
+  
+    if (readExtensionElement(element, "failedJobRetryTimeCycle")) {unsupportedExtensionElement(hints, "Service Task", "failedJobRetryTimeCycle");}
+    if (readExtensionElement(element, "connector")) {unsupportedExtensionElement(hints, "Service Task", "connector");}
+  }
+
+  console.log(element);
+  console.log("------------ ---------- -----------------");
+  return hints;
+}
+
+ /**
+ * ##################
+ * # Receive Task
+ * # 
+ * ##################
+ */
+function convertReceiveTask(element) {
+    var hints = [];
+    console.log("------------ Receive Task -----------------");
+  
+    console.log(element);
+    console.log("------------ ---------- -----------------");
+    return hints;
+}
+
+/**
+ * ##################
+ * # Receive Event
+ * # 
+ * ##################
+ */
 
 ConvertToCamundaCloudPlugin.$inject = [ 'elementRegistry', 'editorActions', 'canvas', 'modeling', 'eventBus', 'commandStack', 'overlays']; // 'bpmnjs'
 
