f257c4adbe0c678553d68616511d74dd83be2d91
Jonathan Lukas <jonathan.lukas@camunda.com>
2022-10-19 13:12:27 +0200

    bug fixed, resultVariable has to handled context-aware

--- backend-diagram-converter/core/src/main/java/org/camunda/community/converter/visitor/impl/attribute/ResultVariableVisitor.java
+++ backend-diagram-converter/core/src/main/java/org/camunda/community/converter/visitor/impl/attribute/ResultVariableVisitor.java
@@ -1,20 +1,51 @@
 package org.camunda.community.converter.visitor.impl.attribute;
 
 import org.camunda.community.converter.DomElementVisitorContext;
+import org.camunda.community.converter.convertible.AbstractDataMapperConvertible;
 import org.camunda.community.converter.convertible.BusinessRuleTaskConvertible;
 import org.camunda.community.converter.visitor.AbstractSupportedAttributeVisitor;
 
-public class ResultVariableVisitor extends AbstractSupportedAttributeVisitor {
+public abstract class ResultVariableVisitor extends AbstractSupportedAttributeVisitor {
   @Override
   public String attributeLocalName() {
     return "resultVariable";
   }
 
   @Override
-  protected String visitSupportedAttribute(DomElementVisitorContext context, String attribute) {
-    context.addConversion(
-        BusinessRuleTaskConvertible.class,
-        conversion -> conversion.getZeebeCalledDecision().setResultVariable(attribute));
-    return "Result variable is a simple string";
+  protected boolean canVisit(DomElementVisitorContext context) {
+    return super.canVisit(context) && canVisitResultVariable(context);
+  }
+
+  protected abstract boolean canVisitResultVariable(DomElementVisitorContext context);
+
+  public static class ResultVariableOnBusinessRuleTaskVisitor extends ResultVariableVisitor {
+    @Override
+    protected String visitSupportedAttribute(DomElementVisitorContext context, String attribute) {
+      context.addConversion(
+          BusinessRuleTaskConvertible.class,
+          conversion -> conversion.getZeebeCalledDecision().setResultVariable(attribute));
+      return "Result variable is a simple string";
+    }
+
+    @Override
+    protected boolean canVisitResultVariable(DomElementVisitorContext context) {
+      return context.getElement().getLocalName().equals("businessRuleTask");
+    }
+  }
+
+  public static class ResultVariableOnRestVisitor extends ResultVariableVisitor {
+
+    @Override
+    protected String visitSupportedAttribute(DomElementVisitorContext context, String attribute) {
+      context.addConversion(
+          AbstractDataMapperConvertible.class,
+          convertible -> convertible.addZeebeTaskHeader("resultVariable", attribute));
+      return "Result variable was mapped as header 'resultVariable'";
+    }
+
+    @Override
+    protected boolean canVisitResultVariable(DomElementVisitorContext context) {
+      return !context.getElement().getLocalName().equals("businessRuleTask");
+    }
   }
 }
--- backend-diagram-converter/core/src/test/java/org/camunda/community/converter/BpmnConverterTest.java
+++ backend-diagram-converter/core/src/test/java/org/camunda/community/converter/BpmnConverterTest.java
@@ -4,24 +4,33 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.ByteArrayInputStream;
 import java.util.Set;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.camunda.bpm.model.xml.impl.util.IoUtil;
 import org.camunda.community.converter.conversion.Conversion;
 import org.camunda.community.converter.visitor.DomElementVisitor;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class BpmnConverterTest {
 
-  @RepeatedTest(100)
-  public void shouldConvert() throws JsonProcessingException {
+  static Stream<Arguments> argProvider() {
+    return Stream.of("example-c7.bpmn", "example-c7_2.bpmn")
+        .flatMap(fileName -> IntStream.range(0, 100).mapToObj(i -> Arguments.of(fileName)));
+  }
+
+  @ParameterizedTest
+  @MethodSource("argProvider")
+  public void shouldConvert(String bpmnFile) throws JsonProcessingException {
     ObjectMapper objectMapper = new ObjectMapper();
     Set<DomElementVisitor> visitors = DomElementVisitorFactory.getInstance().get();
     Set<Conversion> conversions = ConversionFactory.getInstance().get();
     BpmnConverter converter = new BpmnConverter(visitors, conversions);
     BpmnModelInstance modelInstance =
-        Bpmn.readModelFromStream(
-            this.getClass().getClassLoader().getResourceAsStream("example-c7.bpmn"));
+        Bpmn.readModelFromStream(this.getClass().getClassLoader().getResourceAsStream(bpmnFile));
     BpmnDiagramCheckResult result = converter.check(modelInstance, true);
     result =
         objectMapper.readValue(
