b9acbbb5a45968f97a1cac7d727cb5cb9bdc5ea2
Jonathan Lukas <jonathan.lukas@camunda.com>
2022-10-28 15:06:39 +0200

    Adjusted factory to have setter

--- backend-diagram-converter/core/src/main/java/org/camunda/community/converter/AbstractFactory.java
+++ backend-diagram-converter/core/src/main/java/org/camunda/community/converter/AbstractFactory.java
@@ -3,6 +3,10 @@ package org.camunda.community.converter;
 public abstract class AbstractFactory<T> {
   private T instance;
 
+  public void setInstance(T instance) {
+    this.instance = instance;
+  }
+
   protected abstract T createInstance();
 
   public T get() {
