587298c66261c53145ebd287873b3cf515f1c731
Jonathan Lukas <81753891+jonathanlukas@users.noreply.github.com>
2022-11-10 14:56:34 +0100

    Feature: Make Bean Order deterministic (#77)
    * Replaced set of bean with list of bean
    
    * Deleted LoggingVisitor

--- backend-diagram-converter/core/src/test/java/org/camunda/community/converter/BpmnConverterTest.java
+++ backend-diagram-converter/core/src/test/java/org/camunda/community/converter/BpmnConverterTest.java
@@ -3,26 +3,18 @@ package org.camunda.community.converter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.ByteArrayInputStream;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.camunda.bpm.model.xml.impl.util.IoUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class BpmnConverterTest {
 
-  static Stream<Arguments> argProvider() {
-    return Stream.of("example-c7.bpmn", "example-c7_2.bpmn")
-        .flatMap(fileName -> IntStream.range(0, 100).mapToObj(i -> Arguments.of(fileName)));
-  }
-
   @ParameterizedTest
-  @MethodSource("argProvider")
+  @CsvSource(value = {"example-c7.bpmn", "example-c7_2.bpmn"})
   public void shouldConvert(String bpmnFile) throws JsonProcessingException {
     ObjectMapper objectMapper = new ObjectMapper();
     BpmnConverter converter = BpmnConverterFactory.getInstance().get();
