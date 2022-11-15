b3dcd38465fd14715d9863cf468819d3f492e1d1
Jonathan Lukas <jonathan.lukas@camunda.com>
2022-11-15 09:04:40 +0100

    Added manifest controller

---
+++ backend-diagram-converter/webapp/src/main/java/org/camunda/community/converter/webapp/ConverterManifest.java
@@ -0,0 +1,13 @@
+package org.camunda.community.converter.webapp;
+
+public class ConverterManifest {
+  private boolean engineEnabled;
+
+  public boolean isEngineEnabled() {
+    return engineEnabled;
+  }
+
+  public void setEngineEnabled(boolean engineEnabled) {
+    this.engineEnabled = engineEnabled;
+  }
+}
