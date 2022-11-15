aee02812c1e3a83eedcc747ae2c49b92989e5c80
Jonathan Lukas <jonathan.lukas@camunda.com>
2022-11-15 09:04:40 +0100

    Added manifest controller

---
+++ backend-diagram-converter/webapp/src/main/java/org/camunda/community/converter/webapp/ManifestController.java
@@ -0,0 +1,24 @@
+package org.camunda.community.converter.webapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ManifestController {
+  private final boolean engineEnabled;
+
+  @Autowired
+  public ManifestController(@Value("${camunda.bpm.client.enabled}") boolean engineEnabled) {
+    this.engineEnabled = engineEnabled;
+  }
+
+  @GetMapping("/manifest")
+  public ResponseEntity<ConverterManifest> getManifest() {
+    ConverterManifest manifest = new ConverterManifest();
+    manifest.setEngineEnabled(engineEnabled);
+    return ResponseEntity.ok(manifest);
+  }
+}
