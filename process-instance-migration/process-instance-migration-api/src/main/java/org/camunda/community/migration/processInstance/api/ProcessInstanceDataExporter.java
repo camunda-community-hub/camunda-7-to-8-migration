0458bcb73c3342533b030f6ba6d41291d70bedba
Jonathan Lukas <jonathan.lukas@camunda.com>
2023-03-08 14:00:35 +0100

    Defined event listeners for ProcessInstanceDataExporter

--- process-instance-migration/process-instance-migration-api/src/main/java/org/camunda/community/migration/processInstance/api/ProcessInstanceDataExporter.java
+++ process-instance-migration/process-instance-migration-api/src/main/java/org/camunda/community/migration/processInstance/api/ProcessInstanceDataExporter.java
@@ -3,9 +3,18 @@ package org.camunda.community.migration.processInstance.api;
 import org.camunda.community.migration.processInstance.api.model.ProcessInstanceData;
 
 public interface ProcessInstanceDataExporter {
+  /**
+   * @param start
+   * @param limit
+   * @return
+   */
   Page<ProcessInstanceData> list(long start, long limit);
 
   ProcessInstanceData get(String id);
 
-  void delete(String id);
+  void onMigrationStarted(String id);
+
+  void onMigrationSuccess(String id);
+
+  void onMigrationFailed(String id);
 }
