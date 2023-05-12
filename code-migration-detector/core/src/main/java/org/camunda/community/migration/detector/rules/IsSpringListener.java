f5ffde232060acb15e0fc0afea919d475f27551d
Simon Zambrovski <simon.zambrovski@holisticon.de>
2023-05-12 20:13:33 +0200

    spotless format

--- code-migration-detector/core/src/main/java/org/camunda/community/migration/detector/rules/IsSpringListener.java
+++ code-migration-detector/core/src/main/java/org/camunda/community/migration/detector/rules/IsSpringListener.java
@@ -6,18 +6,21 @@ import org.springframework.context.event.EventListener;
 
 /**
  * Predicate matching if the method is a Spring Listener.
+ *
  * @param <T> type of the Spring event.
  */
 public class IsSpringListener<T> extends DescribedPredicate<JavaMethod> {
-	private final Class<T> eventType;
+  private final Class<T> eventType;
 
-	public IsSpringListener(Class<T> eventType) {
-		super("is a Spring Event Listener for " + eventType.getName());
-		this.eventType = eventType;
-	}
+  public IsSpringListener(Class<T> eventType) {
+    super("is a Spring Event Listener for " + eventType.getName());
+    this.eventType = eventType;
+  }
 
-	@Override public boolean test(JavaMethod javaMethod) {
-		return javaMethod.isAnnotatedWith(EventListener.class) && javaMethod.getRawParameterTypes().stream().anyMatch(
-				(clazz) -> eventType.isAssignableFrom(clazz.reflect()));
-	}
+  @Override
+  public boolean test(JavaMethod javaMethod) {
+    return javaMethod.isAnnotatedWith(EventListener.class)
+        && javaMethod.getRawParameterTypes().stream()
+            .anyMatch((clazz) -> eventType.isAssignableFrom(clazz.reflect()));
+  }
 }
