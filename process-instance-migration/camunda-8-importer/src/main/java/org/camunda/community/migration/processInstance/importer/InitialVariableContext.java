4ef59140d45f644b99f00fa29ae896617bbb2444
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-01-27 16:15:48 +0100

    context that holds c7 pi id as value

---
+++ process-instance-migration/camunda-8-importer/src/main/java/org/camunda/community/migration/processInstance/importer/InitialVariableContext.java
@@ -0,0 +1,7 @@
+package org.camunda.community.migration.processInstance.importer;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record InitialVariableContext(@JsonProperty(CAMUNDA_7_PROCESS_INSTANCE_ID_VARIABLE) String camunda7ProcessInstanceId) {
+  public static final String CAMUNDA_7_PROCESS_INSTANCE_ID_VARIABLE = "camunda7ProcessInstanceId";
+}
