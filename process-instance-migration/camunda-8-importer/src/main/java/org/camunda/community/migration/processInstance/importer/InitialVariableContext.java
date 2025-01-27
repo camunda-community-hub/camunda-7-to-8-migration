a0e5424f7b6caf087e9e45f8cd2a26099f6338a9
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-01-27 16:39:44 +0100

    fixed build

--- process-instance-migration/camunda-8-importer/src/main/java/org/camunda/community/migration/processInstance/importer/InitialVariableContext.java
+++ process-instance-migration/camunda-8-importer/src/main/java/org/camunda/community/migration/processInstance/importer/InitialVariableContext.java
@@ -2,6 +2,7 @@ package org.camunda.community.migration.processInstance.importer;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record InitialVariableContext(@JsonProperty(CAMUNDA_7_PROCESS_INSTANCE_ID_VARIABLE) String camunda7ProcessInstanceId) {
+public record InitialVariableContext(
+    @JsonProperty(CAMUNDA_7_PROCESS_INSTANCE_ID_VARIABLE) String camunda7ProcessInstanceId) {
   public static final String CAMUNDA_7_PROCESS_INSTANCE_ID_VARIABLE = "camunda7ProcessInstanceId";
 }
