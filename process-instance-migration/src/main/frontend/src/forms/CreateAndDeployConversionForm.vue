3638b1d36b8f5a3d0f10c70aae200fc8e20275c1
Bernd Ruecker <bernd.ruecker@camunda.com>
2025-03-31 12:27:43 +0200

    Removed process-instance-migration (as it will be replaced by Data Migrator)

--- process-instance-migration/src/main/frontend/src/forms/CreateAndDeployConversionForm.vue
+++
@@ -1,8 +0,0 @@
-<template>
-<span>Please create a process definition with <code>bpmnProcessId: {{ data.bpmnProcessId }}</code> and deploy it to your Camunda 8 instance.</span>
-</template>
-<script setup lang="ts">
-
-defineProps(['data'])
-defineExpose()
-</script>
